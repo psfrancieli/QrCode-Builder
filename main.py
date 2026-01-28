ce889cc3c4bcbd2de7fec5de493ec87565d95dd8
psfrancieli <francielisilva2011@gmail.com>
2026-01-28 11:54:32 -0300

    qrcode e salvar feitos

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
+from PyQt5.QtGui import QPixmap
 from PyQt5.uic import loadUi
 from pyshorteners import Shortener
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QMessageBox
+from os import path
+import qrcode
 
 def CREATE_SHORT_URL(url):
     link = Shortener()
     return link.tinyurl.short(url)
+
+def CREATE_QRCODE(link):
+    img = qrcode.make(link)
+    img.save("qrcode.png")
+
+def GET_LOCAL_PATH(myPath):
+    pass
 
 class QrCode(QMainWindow):
     def __init__(self, **kwargs):
@@ -21,14 +30,37 @@ class QrCode(QMainWindow):
         return self.url2.setText(url)
 
     @pyqtSlot()
-    def on_gerar_released(self):
+    def on_gerar_clicked(self):
         valor = self.getUrl()
 
         if self.getUrl() == "":
             self.showMessage("Erro", "URL inválida!")
+            return
         else:
             url = CREATE_SHORT_URL(valor)
+            CREATE_QRCODE(url)
             self.setShortUrl(url)
+            self.img.setPixmap(QPixmap("qrcode.png"))
+            self.salvar.setEnabled(True)
+
+    @pyqtSlot()
+    def on_salvar_clicked(self):
+        self.salvarArquivo()
+
+    def salvarArquivo(self):
+        nomeArquivo, _ = QFileDialog.getSaveFileName(self, "Salvar Imagem")
+
+        if nomeArquivo:
+            caminho = path.dirname(nomeArquivo)
+            nome = nomeArquivo.removeprefix(caminho)
+
+            #  ler foto
+            with open("qrcode.png", "rb") as fotoQrcode:
+                dadosQrcode = fotoQrcode.read()
+
+            #  salvar foto
+            with open(caminho+f"{nome}.png", "wb") as foto:
+                foto.write(dadosQrcode)
 
     def showMessage(self, title, message):
         QMessageBox.information(self, title, message)
@@ -38,3 +70,5 @@ if __name__ == "__main__":
     app = QApplication([])
     tela = QrCode()
     app.exec_()
+
+# https://roadmap.sh/ai/roadmap
